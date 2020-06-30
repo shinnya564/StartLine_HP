ec6d5795103a72b7ae01247ab92a7eb79106bf76
shinnya <65529573+shinnya564@users.noreply.github.com>
2020-06-30 15:18:12 +0900

    [update]20200630

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,11 +43,24 @@ $(document).ready(function () {
 });
 
 
+//page top up
 $(function() {
-  $('#back a').on('click',function(event){
+  $('#back').on('click',function(event){
     $('body, html').animate({
       scrollTop:0
     }, 800);
     event.preventDefault();
   });
+});
+
+
+//about page tab
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
